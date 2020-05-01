5e906ec82d1d7576ce76af8169355d04fbeba7aa
user169471 <user169471@FF022.local>
2020-05-01 14:27:57 +0200

    initial commit

---
+++ OrtografieQuiz/OrtografieQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OrtografieQuiz
+//
+//  Created by user169471 on 5/1/20.
+//  Copyright © 2020 dcg-351. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

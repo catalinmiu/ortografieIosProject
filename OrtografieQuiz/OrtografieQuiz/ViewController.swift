48238b300a1a5786079d286a5f65404c655a38af
user169471 <user169471@FF022.local>
2020-05-02 23:01:14 +0200

    integrate with firebase

--- OrtografieQuiz/OrtografieQuiz/ViewController.swift
+++ OrtografieQuiz/OrtografieQuiz/ViewController.swift
@@ -7,14 +7,22 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var questionTitle: UILabel!
+//    var asdq: DatabaseReference
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+//        asdq = DatabaseReference..child("Questions")
+    
+        //ref.child("Questions").childByAutoId().setValue(["question": "_____ sunt cuminti", "possibileAnswer1": "Copii", "possibileAnswer2": "Copiii", "correct_answer": 1])
+        //ref.child("Questions").childByAutoId().setValue(["question": "Sa incepi din noaptea aceea, ____ minte", "possibileAnswer1": "tii", "possibileAnswer2": "ti", "correct_answer": 1])
+        //ref.child("Questions").childByAutoId().setValue(["question": "Cum de in cateva zile ____ schimbat astfel locurile", "possibileAnswer1": "s-au", "possibileAnswer2": "sau", "correct_answer": 1])
+        //ref.childByAutoId().setValue(["name": "TOM", "age":14, "gender": "F"])
     }
 
-
+    
 }
 
